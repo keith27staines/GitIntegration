f3c6b63fd0209aceb55f2dd93c83a1cf166812d7
generic-keith <keith.staines.ext@bbc.co.uk>
2022-09-03 10:45:05 +0100

    Initial Commit

---
+++ GitIntegration/GitIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitIntegrationApp.swift
+//  GitIntegration
+//
+//  Created by Keith Staines on 03/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
